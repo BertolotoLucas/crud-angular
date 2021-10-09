e7b1df82ced049bc1285640d8a9a0f3755a578f4
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-10-09 17:26:26 -0300

    Created a base Material Table to list Courses

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  courses: Course[];
+  displayedColumns = ['name','category'];
+
+  constructor() {
+    this.courses = [
+      {_id: 1, name: 'Angular', category: 'front-end'}
+    ];
+  }
 
   ngOnInit(): void {
   }
