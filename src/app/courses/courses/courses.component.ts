62326f15f0c245230151ae79f834f4adba2e1f28
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-10-10 15:53:28 -0300

    Chang. css Tbl Mat & creating Material App module

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
