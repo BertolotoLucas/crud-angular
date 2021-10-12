ba58ad0995046c870658ddf1b235834837ae72d2
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-10-12 14:36:50 -0300

    Created Course Service

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 
 @Component({
@@ -9,16 +12,59 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[];
+  courses?: Course[];
+  selected?: Course;
+  currentIndex: number = -1;
+  name: string = '';
+  category: string = '';
   displayedColumns = ['name','category'];
 
-  constructor() {
-    this.courses = [
-      {_id: 1, name: 'Angular', category: 'front-end'}
-    ];
-  }
+  constructor(private courseService: CoursesService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.getCourses();
+    })
+  }
+
+  getCourses(): void {
+    this.courseService.list()
+      .subscribe( course => {
+        this.courses = course;
+      },
+      error => {
+        console.error(error);
+      });
+  }
+
+  refreshList(): void {
+    this.getCourses();
+    this.selected = undefined;
+    this.currentIndex = -1;
+  }
+
+  setSelected(course: Course, index: number): void {
+    if (this.selected && this.selected._id == course._id) {
+      this.selected = undefined;
+      this.currentIndex = -1;
+    } else {
+      this.selected = course;
+      this.currentIndex = index;
+    }
+  }
+
+  deleteCourse(): void {
+    if (!this.selected) {
+      return;
+    }
+
+    this.courseService.delete(this.selected._id)
+      .subscribe(response => {
+        this.refreshList();
+      },
+      error => {
+        console.error(error);
+      });
   }
 
 }
